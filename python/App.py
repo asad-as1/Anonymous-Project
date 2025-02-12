269f003c82d47f2c196652623f216ea2f336ce15
Mohd Asad Ansari <asad.as9458@gmail.com>
2025-02-13 00:53:45 +0530

    sjd

--- python/App.py
+++ python/App.py
@@ -4,11 +4,13 @@ from TakeATest import take_a_test_bp
 from Summarization import summarization_bp
 
 app = Flask(__name__)
-CORS(app)  # Enable Cross-Origin Resource Sharing
+
+# Allow requests from your frontend domain
+CORS(app, resources={r"/*": {"origins": "https://eduaccess-as2.netlify.app"}})
 
 # Register Blueprints
 app.register_blueprint(take_a_test_bp)
 app.register_blueprint(summarization_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True , use_reloader=False)
+    app.run(host="0.0.0.0", port=5000)
